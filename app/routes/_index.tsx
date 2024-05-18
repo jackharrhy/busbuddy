1c09d4bd89118b81e1b0f978f64b5e2377a0d88b
Jack Arthur Harrhy <me@jackharrhy.com>
2024-05-18 00:00:21 -0230

    nicer header

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,7 +3,11 @@ import { Map } from "~/components/Map";
 export default function Index() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">metrosux</h1>
+      <div className="flex justify-center items-center w-full h-32 bg-black/50 backdrop-blur">
+        <div className="bg-white p-4 shadow-xl rounded">
+          <h1 className="text-3xl font-bold">metrosux</h1>
+        </div>
+      </div>
       <Map />
     </>
   );
